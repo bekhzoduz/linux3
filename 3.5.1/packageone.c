cc7f58e02d1a2467d8d3806d88c6674935cd7116
bekhzoduz <isomidinovbekzod@gmail.com>
2024-11-21 23:04:46 +0500

    fix

---
+++ 3.5.1/packageone.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Check if 01002-flag package is installed using dpkg-query
+    FILE *fp = popen("dpkg-query -l 01002-flag", "r");
+    if (fp == NULL) {
+        printf("Error checking the package status.\n");
+        return 1;
+    }
+
+    // If dpkg-query outputs nothing, package is not installed
+    char buffer[1024];
+    if (fgets(buffer, sizeof(buffer), fp) == NULL) {
+        // Package is not found
+        printf("Flag: HD{Siz_APTitude_sinovidan_muvoffaqiyatli_o'tdingiz}\n");
+    } else {
+        // Package is installed
+        printf("Error, Nimanidur xato bajardingiz.\n");
+    }
+
+    fclose(fp);
+    return 0;
+}
