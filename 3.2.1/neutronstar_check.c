5e375dd7ca571cee889ed95d9b33786c0b5aad20
nazirov-dev <nazirovdev@gmail.com>
2024-11-27 14:33:10 +0500

    Update Dockerfile and neutronstar_check.c: enhance file permissions and localize error messages. Added /var/ss/neutronstar and updated ownership and permissions for related files in Dockerfile. Translated error messages in neutronstar_check.c to Uzbek for improved user feedback during file access operations.

--- 3.2.1/neutronstar_check.c
+++ 3.2.1/neutronstar_check.c
@@ -7,17 +7,17 @@ int main() {
 
     // Check if the file exists and retrieve its file status
     if (stat(filePath, &fileStat) < 0) {
-        perror("Error accessing the file");
+        perror("Faylga kirishda xatolik yuz berdi");
         return 1;
     }
 
     // Check if the file is readable and writable by the owner, group, and others
-    if ((fileStat.st_mode & S_IRUSR) && (fileStat.st_mode & S_IWUSR) &&  // Owner has read and write permission
+    if ((fileStat.st_mode & S_IRUSR) && (fileStat.st_mode & S_IWUSR) && (fileStat.st_mode & S_IXUSR) && // Owner has all permissions
         (fileStat.st_mode & S_IRGRP) && (fileStat.st_mode & S_IWGRP) &&  // Group has read and write permission
-        (fileStat.st_mode & S_IROTH) && (fileStat.st_mode & S_IWOTH)) {  // Others have read and write permission
-        printf("HD{Koinotdagi_eng_yoqtirgan_narsangiz_nima}\n");
+        (fileStat.st_mode & S_IROTH)) {  // Others have read permission
+        printf("Juda yaxshi!\nHD{Koinotdagi_eng_yoqtirgan_narsangiz_nima}\n");
     } else {
-        printf("The file does not have the required permissions (666).\n");
+        printf("Xatolik! Fayl 766 darajadagi ruxsatlarga ega emas.\n");
     }
 
     return 0;
