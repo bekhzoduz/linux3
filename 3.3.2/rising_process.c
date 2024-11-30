9e20279536d7e65e89f538442977aee3aaedd9d4
nazirov-dev <nazirovdev@gmail.com>
2024-11-30 15:23:59 +0500

    Refactor rising_process.c to improve user interaction and process control: Changed the loop structure to use a 'done' flag for better clarity in menu handling. This update enhances the program's responsiveness to user choices, allowing for smoother execution and termination of processes.

--- 3.3.2/rising_process.c
+++ 3.3.2/rising_process.c
@@ -29,24 +29,31 @@ int main() {
     int choice = 0;
 
     // Menyuni chiqarish va foydalanuvchidan tanlov so'rash
-    while (1) {
+    int done = 1;
+    while (done) {
         print_menu();
         scanf("%d", &choice);
 
         // Tanlovni tekshirish
+
         switch (choice) {
             case 1:
                 display_flag();
+                done = 0;
                 break;
             case 2:
                 printf("Chiqish...\n");
+                done = 0;
                 exit(0);
             case 3:
                 printf("Processni backgroundga o'tkazish...\n");
                 kill(getpid(), SIGSTOP); // Processni to'xtatish
+                done = 0;
                 break;
             default:
                 printf("Xato tanlov! Iltimos, qayta urinib ko'ring.\n");
+                done = 0;
+                break;
         }
     }
 
