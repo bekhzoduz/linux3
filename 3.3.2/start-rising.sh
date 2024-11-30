56ecb1f6db91568f8cb46697d3c3a52e77df018b
nazirov-dev <nazirovdev@gmail.com>
2024-11-30 11:06:07 +0500

    Refactor Dockerfile and scripts for improved process management: Updated .bashrc to comment out the previous start command and added new commands to manage the 'rising' process. Modified start-rising.sh to start 'rising' in the background without an initial argument and suspend it using SIGSTOP. Adjusted rising_process.c for better user interaction by refining the default case in the switch statement. These changes enhance clarity and streamline the startup process in the container.

--- 3.3.2/start-rising.sh
+++ 3.3.2/start-rising.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-set -m
 
 # Start /usr/bin/rising and simulate pressing "3" to send it to the background
-/usr/bin/rising 3 &
+/usr/bin/rising &
 
 # Now, kill the process by sending a STOP signal to it (this will suspend it)
 kill -SIGSTOP $!
