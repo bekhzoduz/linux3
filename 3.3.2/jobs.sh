f942d68299fb7e026a3ee9e08abe04ed2ac7b013
nazirov-dev <nazirovdev@gmail.com>
2024-11-30 18:53:21 +0500

    Update jobs.sh to include completion message for ASHES_DONE flag: Added a conditional echo statement to notify users of successful completion when the ASHES_DONE variable is set. This enhancement improves user feedback and interaction within the job management script.

--- 3.3.2/jobs.sh
+++ 3.3.2/jobs.sh
@@ -6,4 +6,5 @@ if [ "$(command jobs)" ]; then
 else
     if [ "$ASHES_DONE" == "1" ]; then
         echo "\n\nPhoenix@Haady dan translatsiya xabari, (qayerdandir) $(date '+%A, %B %d, %H:%M:%S %Y'):\n\nXatosiz g'alaba qozondingiz! Flag:\nHD{Men_yo'qolgan_bo'lishim_mumkin, lekin_qaytaman, mana_siz_hali_ham_orzu_qilayotgan_flag}"
+    fi
 fi
