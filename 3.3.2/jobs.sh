6e14a192ba00e7179369173b1f3f72a853521167
nazirov-dev <nazirovdev@gmail.com>
2024-11-30 18:57:59 +0500

    Refactor jobs.sh for improved user feedback: Updated conditional checks to provide clearer messages regarding job status. Enhanced output for the ASHES_DONE variable to inform users of completion status, improving interaction within the job management script.

--- 3.3.2/jobs.sh
+++ 3.3.2/jobs.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Tekshiruv - jobs buyruq natijasi bo'sh bo'lsin
-if [ "$(command jobs)" ]; then
-   command jobs
+if [ -z "$(jobs)" ]; then
+    echo "jobs bo'sh emas"
+    jobs_output=$(jobs)
+    echo "$jobs_output"
 else
+    echo "jobs bo'sh"
     if [ "$ASHES_DONE" == "1" ]; then
+        echo "ASHES_DONE 1"
         echo "\n\nPhoenix@Haady dan translatsiya xabari, (qayerdandir) $(date '+%A, %B %d, %H:%M:%S %Y'):\n\nXatosiz g'alaba qozondingiz! Flag:\nHD{Men_yo'qolgan_bo'lishim_mumkin, lekin_qaytaman, mana_siz_hali_ham_orzu_qilayotgan_flag}"
+    else
+        echo "ASHES_DONE 0"
     fi
 fi
