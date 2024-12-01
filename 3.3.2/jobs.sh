57ca58705797304a32d680ea8fef75862cb20831
nazirov-dev <nazirovdev@gmail.com>
2024-12-01 14:06:27 +0500

    Enhance jobs.sh to include job listing before completion message: Added a command to list current jobs when the ASHES_DONE flag is set, improving user feedback and interaction. This change ensures users are informed of active jobs prior to receiving the success notification, contributing to a more informative job management experience.

--- 3.3.2/jobs.sh
+++ 3.3.2/jobs.sh
@@ -6,6 +6,7 @@ if [ -f /tmp/jobs_output ]; then
         if [ -f /home/phoenix/.ashes_done ]; then
             ASHES_DONE=$(cat /home/phoenix/.ashes_done)
             if [ $ASHES_DONE == "ASHES_DONE=1" ]; then
+                $(jobs)
                 echo $'\n\nPhoenix@Haady dan translatsiya xabari, (qayerdandir) '$(date '+%A, %B %d, %H:%M:%S %Y')$':\n\nXatosiz g\'alaba qozondingiz! Flag:\nHD{Men_yo\'qolgan_bo\'lishim_mumkin, lekin_qaytaman, mana_siz_hali_ham_orzu_qilayotgan_flag}\n'
                 rm -f /home/phoenix/.ashes_done
             fi
