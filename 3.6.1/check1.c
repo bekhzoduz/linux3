98f65922624ecdeed1d9c3f2c76ec7ea9d0e2a82
nazirov-dev <nazirovdev@gmail.com>
2024-12-01 20:05:36 +0500

    Refactor check1.c to read from /etc/crontab and enhance cron job detection: Updated the cron job search logic to include both specific time and day formats. Modified output messages for clarity. Updated Dockerfile to compile check1.c into a new directory structure, improving organization of compiled binaries.

--- 3.6.1/check1.c
+++ 3.6.1/check1.c
@@ -7,16 +7,16 @@ int main() {
     char line[256];
     int cron_found = 0;
 
-    // Execute 'crontab -l' to list user cron jobs
-    fp = popen("crontab -l", "r");
+    // Open /etc/crontab file
+    fp = fopen("/etc/crontab", "r");
     if (fp == NULL) {
-        perror("Failed to run crontab -l");
+        perror("Failed to open /etc/crontab");
         return 1;
     }
 
     // Search for the specific cron job entry
     while (fgets(line, sizeof(line), fp) != NULL) {
-        if (strstr(line, "37 13 * * 3")) {
+        if (strstr(line, "37 13 * * 3") || strstr(line, "37 13 * * wed")) {
             printf("Flag:\nHD{Anacron_cronga_o‘xshaydi,_lekin_tizim_vaqti-vaqti_bilan_o‘chirilishini_faraz_qiladi}\n");
             cron_found = 1;
             break;
@@ -24,7 +24,7 @@ int main() {
     }
 
     if (!cron_found) {
-        printf("Kerakli cron job topilmadis.\n");
+        printf("Vazifa bajarilmadi.\n");
     }
 
     fclose(fp);
